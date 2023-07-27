027a1b063756094472de6db5dd891ff855194b12
svat <svat12312@gmail.com>
2023-07-27 14:27:18 +0300

    First commit

---
+++ main.py
@@ -0,0 +1,16 @@
+import discord
+from discord.ext import commands
+import music
+
+cogs = [music]
+
+client = commands.Bot(command_prefix='+', intents = discord.Intents.all())
+
+for i in range(len(cogs)):
+    cogs[i].setup(client)
+
+client.run("#token")
+
+#python3 -m poetry add PyNaCl
+
+
